c710956fda5f17db60f7afd9fba955159cd93134
Chris Gilmer <chris.gilmer@gmail.com>
2011-11-20 13:48:23 -0800

    Made the api work.  Added in initial data.  Moved the images

--- project/apps/shredder/views.py
+++ project/apps/shredder/views.py
@@ -2,22 +2,18 @@ import simplejson as json
 
 from django.http import HttpResponse
 
-#from project.apps.shredder.models import Pair
+from project.apps.shredder.models import Pair
 
 
 def pair_api(request):
 
-    content = {
-                "images": [
-                  {
-                    "id": "42",
-                    "src": "./static/images/test.png"
-                  },
-                  {
-                    "id": "43",
-                    "src": "./static/images/test.png"
-                  }
-                ]
-            }
+    pair = Pair.objects.get(id=1)
+    images = []
+    for image in [pair.piece1, pair.piece2]:
+        d = {}
+        d['id'] = image.hash_id
+        d['src'] = "./static/images/%s.png" % image.hash_id
+        images.append(d)
+    content = {"images": images}
     content = json.dumps(content)
     return HttpResponse(content, mimetype='application/json')
