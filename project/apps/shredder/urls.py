fb0b639b1c4f81e69b012b357bc7d24fd2387c8c
Chris Gilmer <chris.gilmer@gmail.com>
2011-11-20 17:57:08 -0800

    Renamed api view for vote.

--- project/apps/shredder/urls.py
+++ project/apps/shredder/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('project.apps.shredder.views',
     url(r'pair_api/', 'pair_api', name='pair_api'),
-    url(r'vote/', 'vote', name='vote'),
+    url(r'vote_api/', 'vote_api', name='vote_api'),
 )
